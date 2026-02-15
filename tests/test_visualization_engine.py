eab15dcda3f942a51006226c6c92f11196602a75
Nzati stephane <stephnzati@gmail.com>
2026-02-15 17:51:20 +0100

    Add: Creation de la classe de tests

--- tests/test_visualization_engine.py
+++ tests/test_visualization_engine.py
@@ -14,3 +14,7 @@ sys.path.insert(0, str(Path(__file__).parent.parent / "src"))
 from visualization_engine import VisualizationEngine
 
 
+class TestVisualizationEngine:
+    """Tests for VisualizationEngine class."""
+
+    
