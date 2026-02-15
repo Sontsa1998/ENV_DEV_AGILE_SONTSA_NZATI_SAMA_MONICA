bdf4b950199a6d08e0211a456937a20ec1542a8c
Sontsa1998 <christlove700@gmail.com>
2026-02-15 16:28:53 +0100

    add: filter_engine.py

---
+++ src/filter_engine.py
@@ -0,0 +1,9 @@
+"""
+Filter Engine module for dynamic data filtering.
+
+This module provides functions to build and apply dynamic filters to SQL queries
+and retrieve available filter options.
+"""
+
+from typing import Dict, List, Any, Optional, Tuple
+from database_manager import DatabaseManager
